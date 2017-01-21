74443b526a9f38ca4c3e4b78d99e2778fa8855d7
Felipe Gavilan <elmejor_887@hotmail.com>
2017-01-21 19:16:43 -0400

    Add project files.

---
+++ EjemploTwilio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjemploTwilio.Startup))]
+namespace EjemploTwilio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
